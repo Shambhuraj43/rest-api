7e6fbd84815a8670486d1578e0e7d0ab50598a50
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-07 21:44:05 -0500

    created `/drinks` route

--- application.py
+++ application.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello'
+    return 'Hello!'
+
+@app.route('/drinks')
+def get_drinks():
+    # return "hello drinks"
+    return {"drinks": "drink data"}
+
+if __name__ == "__main__":
+  app.run()
